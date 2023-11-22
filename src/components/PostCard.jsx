cf27019243ad199edae4b3108d1d41cbb7c0b817
Sanket <sanketkarki2060@gmail.com>
2023-11-22 15:28:17 +0545

    consolelogmessagesadded

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AppwriteService from '../appwrite/config'
 import { Link } from 'react-router-dom'
 function PostCard({$id,title,featuredimage}) {
+  useEffect(()=>{  
+    console.log(`Data:`,$id,title,featuredimage);  //Why is it showing  undefined??
+  },[]);
   return (
-    <Link to={`/posts/${id}`}>
+    <Link to={`/posts/${$id}`}>
        <div className='w-full bg-gray-100 rounded-xl p-4'>
          <div className='w-full justify-center mb-4'>
-            <img src={AppwriteService.getFilePreview(featuredimage)} alt={title} srcset="" className='rounded-xl' />
+            <img src={AppwriteService.getFilePreview(`${featuredimage}`)} alt={title} srcset="" className='rounded-xl' />
          </div>
           <h2 className='text-xl font-bold'>{title}</h2>
        </div>
--- src/pages/Allposts.jsx
+++ src/pages/Allposts.jsx
@@ -1,22 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container,PostCard } from '../components'
 import service from '../appwrite/config'
 function Allposts() {
     const [posts,setPosts]=useState([]);
-    service.getPosts(([])
+    useEffect(()=>{
+      service.getPosts()
     .then((posts)=>{
        if (posts){
         setPosts(posts.documents);
        }
-    }))
+    })
+    },[])
+    
+    useEffect(() => {
+      console.log(posts);
+      console.log(posts[0].$id);
+      console.log(posts[0].featuredimage);
+
+    }, [posts])
+    
   return (
     <div className='w-full py-8'>
         <Container>
-          {posts.map((post)=>{
-            <div key={post.$id} className='p-2 w-1/4'>
+          {posts.map((post)=>(   //why with brackets without return statement works->() means return
+          //even without return statement
+          <div key={post.$id} className='p-2 w-1/4'>
               <PostCard post={post}/>
             </div>
-          })}
+            ))}
 
         </Container>
 
